786feec8618f9d3ae46033ed4cbffb127d36867c
Deepika Shukla <deepika.shukla@intelegencia.com>
2025-10-28 16:37:04 +0530

    changes

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@ module.exports = {
   default: {
     require: [
       'hooks/hooks.js',
-      'features/step_definitions/utm_steps.js',
+      'features/step_definitions/utm_step.js',
     ],
     
     paths: ['features/utm_check.feature'],
